30311989dae356f5595455d927cc0a5a8bc819d2
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-04-19 01:56:32 +0200

    Configure variables and fonts

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ export default {};
 </script>
 
 <style lang="scss">
-@import "../src/css/fonts.scss";
+@import "../src/css/_variables.scss";
 @import "../src/css/main.scss";
+@import "../src/css/fonts.scss";
 </style>
