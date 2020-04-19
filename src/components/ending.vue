63ccfd368c03225c16fb28c8c55845b8ee69cf7c
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-04-19 23:14:52 +0200

    Mise en forme ending page

--- src/components/ending.vue
+++ src/components/ending.vue
@@ -3,15 +3,37 @@
     <div class="columns">
       <div class="column">
         <div class="hero is-fullheight page__container">
-          <div class="columns">
+          <div class="columns is-centered">
             <div class="column is-two-thirds ending__column">
-              <div></div>
+              <div>
+                <div class="ending__artwork">
+                  <img src="../assets/sink-artwork.jpg" alt="Malmö Sink EP" />
+                </div>
+                <div class="ending__content">
+                  <h3 class="ending__content__title">
+                    {{ epLabel }}
+                  </h3>
+                  <div class="ending__links">
+                    <a
+                      v-for="link in epLinks"
+                      :href="link.href"
+                      :key="link.service"
+                      >{{ link.service }}</a
+                    >
+                  </div>
+                </div>
+                <div class="ending__btn">
+                  <router-link :to="{ name: 'Chose' }" class="btn"
+                    >{{ button }}
+                  </router-link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div class="column is-5">
-        <div class="hero is-fullheight page__container ending__credits">
+        <div class="hero is-fullheight page__container credits">
           <div class="columns is-centered">
             <div class="column is-two-thirds ending__column">
               <div>
@@ -47,10 +69,34 @@
 export default {
   data() {
     return {
+      button: "chose another track",
+      epLabel: "Sink EP also available on",
+      epLinks: [
+        {
+          service: "bandcamp",
+          href: "#"
+        },
+        {
+          service: "spotify",
+          href: "#"
+        },
+        {
+          service: "soundcloud",
+          href: "#"
+        },
+        {
+          service: "applemusic",
+          href: "#"
+        },
+        {
+          service: "deezer",
+          href: "#"
+        }
+      ],
       title: "Crédits",
       credits: [
         {
-          title: "Music : Malmö",
+          title: "music : Malmö",
           key: "music",
           links: [
             {
@@ -68,7 +114,7 @@ export default {
           ]
         },
         {
-          title: "Photo : Arthur Redler",
+          title: "photo : Arthur Redler",
           key: "photo",
           links: [
             {
@@ -78,6 +124,20 @@ export default {
             {
               label: "instagram",
               href: "https://www.instagram.com/arthurredler/"
+            }
+          ]
+        },
+        {
+          title: "artwork : Gloriakitsune",
+          key: "artwork",
+          links: [
+            {
+              label: "instagram",
+              href: "https://www.instagram.com/gloriakitsune/"
+            },
+            {
+              label: "twitter",
+              href: "https://twitter.com/gloriakitsune"
             }
           ]
         }
