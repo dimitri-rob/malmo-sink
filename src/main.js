d27417304ede271470cee305781786716603d7e1
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-04-19 02:38:15 +0200

    Configure fontawesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import Bulma from "bulma";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import Homepage from "./components/homepage";
 import Chose from "./components/chose";
@@ -10,6 +13,10 @@ import Ending from "./components/ending.vue";
 
 Vue.use(VueRouter);
 Vue.use(Bulma);
+library.add(faAngleRight);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.config.productionTip = false;
 
 const router = new VueRouter({
   mode: "history",
