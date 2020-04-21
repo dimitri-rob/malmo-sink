2bf61e8eddb4845aaf075d1a91eaed0c28390834
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-04-22 01:24:57 +0200

    Add overlay ios fallback

---
+++ src/components/_track-overlay.vue
@@ -0,0 +1,39 @@
+<template>
+  <transition name="fade">
+    <section class="trackOverlay" v-if="overlay">
+      <div class="container">
+        <div class="trackOverlay__infos">
+          <h1 class="trackOverlay__track">{{ track }}</h1>
+          <h2 class="trackOverlay__place">{{ place }}</h2>
+        </div>
+
+        <div class="trackOverlay__btn">
+          <button @click="play" class="btn">Play</button>
+        </div>
+      </div>
+    </section>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: ["track", "place"],
+  data() {
+    return {
+      overlay: true
+    };
+  },
+  methods: {
+    play: function() {
+      this.overlay = false;
+      return this.$parent.sound.play();
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../css/_variables.scss";
+@import "../css/track-overlay.scss";
+@import "../css/transitions.scss";
+</style>
--- src/components/page.track.vue
+++ src/components/page.track.vue
@@ -1,142 +1,157 @@
 <template>
-	<div>
-		<section class="track">
-			<Viewer
-				v-if="ready"
-				:track="track"
-				:path="trackPath"
-				:nbrPhotos="nbrPhotos"
-				:duration="duration"
-			></Viewer>
+  <div>
+    <section class="track">
+      <Overlay :track="track" :place="place" v-if="ios"></Overlay>
 
-			<Control></Control>
+      <Viewer
+        v-if="ready"
+        :track="track"
+        :path="trackPath"
+        :nbrPhotos="nbrPhotos"
+        :duration="duration"
+      ></Viewer>
 
-			<div class="track__oscillo" :style="'opacity:' + (level + 0.05)">
-				<canvas class="track__oscillo__canvas" ref="canvas"></canvas>
-			</div>
+      <Control></Control>
 
-			<Progress :progressBar="progressBar"></Progress>
-		</section>
-	</div>
+      <div class="track__oscillo" :style="'opacity:' + (level + 0.05)">
+        <canvas class="track__oscillo__canvas" ref="canvas"></canvas>
+      </div>
+
+      <Progress :progressBar="progressBar"></Progress>
+    </section>
+  </div>
 </template>
 
 <script>
 import Meyda from "meyda";
 import { Howl, Howler } from "howler";
 import Oscilloscope from "oscilloscope";
+import iOS from "is-ios";
+
 import Viewer from "./_viewer";
 import Progress from "./_progress";
 import Control from "./_control";
+import Overlay from "./_track-overlay";
 
 export default {
-	components: { Viewer, Progress, Control },
-	data() {
-		return {
-			trackPath: "/static/",
-			track: this.$route.params.id,
-			sound: "",
-			seconds: 1,
-			progressBar: 0,
-			pulse: "",
-			audioSource: "",
-			audioContext: "",
-			scope: "",
-			oscContext: "",
-			ready: false,
-			level: ""
-		};
-	},
-	beforeMount() {
-		this.sound = new Howl({
-			src: [this.trackPath + this.$route.params.id + ".mp3"],
-			autoplay: false,
-			html5: true
-		});
-		Howler.autoSuspend = false;
+  components: { Viewer, Progress, Control, iOS, Overlay },
+  data() {
+    return {
+      trackPath: "/static/",
+      track: this.$route.params.id,
+      place: "",
+      sound: "",
+      seconds: 1,
+      progressBar: 0,
+      pulse: "",
+      audioSource: "",
+      audioContext: "",
+      scope: "",
+      oscContext: "",
+      ready: false,
+      level: "",
+      ios: false
+    };
+  },
+  beforeMount() {
+    this.sound = new Howl({
+      src: [this.trackPath + this.$route.params.id + ".mp3"],
+      autoplay: false,
+      html5: true
+    });
+    Howler.autoSuspend = false;
 
-		if (this.track === "sink") {
-			this.nbrPhotos = 12;
-			this.duration = 631.4;
-		} else if (this.track === "wander") {
-			this.nbrPhotos = 8;
-			this.duration = 441.8;
-		} else if (this.track === "boreal") {
-			this.nbrPhotos = 5;
-			this.duration = 126;
-		} else if (this.track === "atlas") {
-			this.nbrPhotos = 9;
-			this.duration = 572.4;
-		}
-	},
-	mounted() {
-		let self = this;
+    if (this.track === "sink") {
+      this.nbrPhotos = 12;
+      this.duration = 631.4;
+      this.place = "Morro Strand State Beach";
+    } else if (this.track === "wander") {
+      this.nbrPhotos = 8;
+      this.duration = 441.8;
+      this.place = "Death Valley";
+    } else if (this.track === "boreal") {
+      this.nbrPhotos = 5;
+      this.duration = 126;
+      this.place = "Sequoia National Park";
+    } else if (this.track === "atlas") {
+      this.nbrPhotos = 9;
+      this.duration = 572.4;
+      this.place = "Grand Canyon";
+    }
+  },
+  mounted() {
+    let self = this;
 
-		this.sound.on("load", function() {
-			this.play();
-		});
+    this.sound.on("load", function() {
+      if (iOS) {
+        self.ios = iOS;
+      } else {
+        this.play();
+      }
+    });
 
-		this.sound.on("play", function() {
-			console.log("Start!");
-			self.ready = true;
+    this.sound.on("play", function() {
+      console.log("Start!");
+      self.ready = true;
 
-			self.audioContext = Howler.ctx;
-			self.audioSource = self.audioContext.createMediaElementSource(
-				this._sounds[0]._node
-			);
-			self.audioSource.connect(Howler.ctx.destination);
+      self.audioContext = Howler.ctx;
+      self.audioSource = self.audioContext.createMediaElementSource(
+        this._sounds[0]._node
+      );
+      self.audioSource.connect(Howler.ctx.destination);
 
-			const analyzer = Meyda.createMeydaAnalyzer({
-				audioContext: self.audioContext,
-				source: self.audioSource,
-				bufferSize: 512,
-				featureExtractors: ["rms"],
-				callback: features => {
-					self.level = features.rms;
-				}
-			});
-			analyzer.start();
+      const analyzer = Meyda.createMeydaAnalyzer({
+        audioContext: self.audioContext,
+        source: self.audioSource,
+        bufferSize: 512,
+        featureExtractors: ["rms"],
+        callback: features => {
+          self.level = features.rms;
+        }
+      });
+      analyzer.start();
 
-			self.scope = new Oscilloscope(self.audioSource);
-			self.oscContext = self.$refs.canvas.getContext("2d");
-			self.oscContext.imageSmoothingQuality = "high";
-			self.oscContext.lineWidth = 0.4;
-			self.oscContext.canvas.width = screen.width;
-			self.oscContext.canvas.height = 130;
-			self.oscContext.strokeStyle = "#ffffff";
-			self.oscContext.fillStyle = "#ffffff";
-			self.scope.animate(self.oscContext);
+      self.scope = new Oscilloscope(self.audioSource);
+      self.oscContext = self.$refs.canvas.getContext("2d");
+      self.oscContext.imageSmoothingQuality = "high";
+      self.oscContext.lineWidth = 0.4;
+      self.oscContext.canvas.width = screen.width;
+      self.oscContext.canvas.height = 130;
+      self.oscContext.strokeStyle = "#ffffff";
+      self.oscContext.fillStyle = "#ffffff";
+      self.scope.animate(self.oscContext);
 
-			self.progress();
+      self.progress();
 
-			this.$interval = setInterval(function() {
-				self.count();
-				self.progress();
-			}, 1000);
-		});
+      this.$interval = setInterval(function() {
+        self.count();
+        self.progress();
+      }, 1000);
+    });
 
-		this.sound.on("end", function() {
-			console.log("Finished!");
-			self.progressBar = 100;
-			clearInterval(this.$interval);
-			this.unload();
-			self.goToEnding();
-		});
-	},
-	beforeDestroy() {
-		this.sound.stop();
-		clearInterval(this.sound.$interval);
-	},
-	methods: {
-		count: function() {
-			return this.seconds++;
-		},
-		progress: function() {
-			return (this.progressBar = (this.seconds * 100) / this.duration);
-		},
-		goToEnding: function() {
-			return this.$router.push({ name: "Ending" });
-		}
-	}
+    this.sound.on("end", function() {
+      console.log("Finished!");
+      self.progressBar = 100;
+      clearInterval(this.$interval);
+      this.unload();
+      self.goToEnding();
+    });
+  },
+  beforeDestroy() {
+    this.sound.stop();
+    clearInterval(this.sound.$interval);
+  },
+  methods: {
+    count: function() {
+      return this.seconds++;
+    },
+    progress: function() {
+      return (this.progressBar = (this.seconds * 100) / this.duration);
+    },
+    goToEnding: function() {
+      return this.$router.push({ name: "Ending" });
+    }
+  }
 };
 </script>
 
