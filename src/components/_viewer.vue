fca926496555f97be46bee5121484cc822da1cbc
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-04-22 22:59:50 +0200

    Update _viewer.vue

--- src/components/_viewer.vue
+++ src/components/_viewer.vue
@@ -1,62 +1,65 @@
 <template>
-	<div>
-		<div class="infiniteZoom">
-			<div
-				class="infiniteZoom__item"
-				v-for="photo in nbrPhotos"
-				:key="photo"
-				v-if="currentImage >= photo"
-				v-show="currentImage <= photo + 2"
-				:style="'animation-duration:' + timePerScale + 's;'"
-			>
-				<img class="infiniteZoom__img" :src="path + track +'/' + photo + '.jpg'" />
-			</div>
-		</div>
-	</div>
+  <div>
+    <div class="infiniteZoom">
+      <figure
+        class="infiniteZoom__item"
+        v-for="photo in nbrPhotos"
+        :key="photo"
+        v-if="currentImage >= photo"
+        v-show="currentImage <= photo + 2"
+        :style="'animation-duration:' + timePerScale + 's;'"
+      >
+        <img
+          class="infiniteZoom__img"
+          :src="path + track + '/' + photo + '.jpg'"
+        />
+      </figure>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			zoom: 1,
-			currentImage: 0,
-			timePerImg: "",
-			timePerScale: ""
-		};
-	},
-	props: {
-		track: {
-			type: String,
-			default: "sink"
-		},
-		nbrPhotos: {
-			type: Number,
-			default: 12
-		},
-		duration: {
-			type: Number,
-			default: 631.4
-		},
-		path: {
-			type: String,
-			default: "/static/"
-		}
-	},
-	mounted() {
-		const self = this;
-		this.timePerImg = Math.round(this.duration / (this.nbrPhotos + 1));
-		this.timePerScale = Math.round(this.timePerImg * 2);
-		self.currentImage++;
+  data() {
+    return {
+      zoom: 1,
+      currentImage: 0,
+      timePerImg: "",
+      timePerScale: ""
+    };
+  },
+  props: {
+    track: {
+      type: String,
+      default: "sink"
+    },
+    nbrPhotos: {
+      type: Number,
+      default: 12
+    },
+    duration: {
+      type: Number,
+      default: 631.4
+    },
+    path: {
+      type: String,
+      default: "/static/"
+    }
+  },
+  mounted() {
+    const self = this;
+    this.timePerImg = Math.round(this.duration / (this.nbrPhotos + 1));
+    this.timePerScale = Math.round(this.timePerImg * 2);
+    self.currentImage++;
 
-		let zoomInterval = setInterval(function() {
-			if (self.currentImage <= self.nbrPhotos) {
-				self.currentImage++;
-			} else {
-				clearInterval(zoomInterval);
-			}
-		}, this.timePerImg * 1000);
-	}
+    let zoomInterval = setInterval(function() {
+      if (self.currentImage <= self.nbrPhotos) {
+        self.currentImage++;
+      } else {
+        clearInterval(zoomInterval);
+      }
+    }, this.timePerImg * 1000);
+  }
 };
 </script>
 
