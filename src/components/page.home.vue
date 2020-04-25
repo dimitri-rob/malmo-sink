ff3305ce11fae2a06c90eda84621f6482b327da5
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-04-25 21:18:02 +0200

    FadeIn transition homepage

--- src/components/page.home.vue
+++ src/components/page.home.vue
@@ -1,20 +1,26 @@
 <template>
-  <section class="page container">
+  <section class="page container homepage" :class="show">
     <div class="hero is-fullheight page__container">
       <div class="columns is-centered is-vcentered">
         <div class="column is-4">
-          <img src="../assets/sink-artwork.jpg" alt="Malmö Sink EP" />
+          <img
+            src="../assets/sink-artwork.jpg"
+            alt="Malmö Sink EP"
+            class="homepage__artwork homepage__fade"
+          />
         </div>
         <div class="column is-2"></div>
         <div class="column is-4">
           <div class="homepage__content">
-            <h1 class="homepage__title">{{ title }}</h1>
-            <h2 class="homepage__subtitle">{{ subtitle }}</h2>
-            <p class="homepage__intro">{{ intro }}</p>
-            <router-link :to="{ name: 'Tracklist' }" class="btn btn--icon">
-              {{ button }}
-              <font-awesome-icon icon="angle-right" />
-            </router-link>
+            <h1 class="homepage__title homepage__fade">{{ title }}</h1>
+            <h2 class="homepage__subtitle homepage__fade">{{ subtitle }}</h2>
+            <p class="homepage__intro homepage__fade">{{ intro }}</p>
+            <div class="homepage__btn homepage__fade">
+              <router-link :to="{ name: 'Tracklist' }" class="btn btn--icon">
+                {{ button }}
+                <font-awesome-icon icon="angle-right" />
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -31,11 +37,15 @@ export default {
       subtitle: "a listening experience",
       intro:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed finibus felis. Aliquam erat volutpat. Donec quis sagittis eros. Pellentesque ullamcorper, libero id egestas feugiat, mauris ante sodales mauris, quis tristique neque lorem ut elit. Quisque turpis nisi, finibus eget eros at, consectetur aliquet justo. Integer auctor ligula purus, sed pretium urna luctus eget. Donec eu placerat felis, id euismod nisl. Maecenas ut ornare ipsum. Duis sed eleifend nunc.",
-      button: "start"
+      button: "start",
+      show: ""
     };
   },
   beforeMount() {
     this.loaded = false;
+  },
+  mounted() {
+    this.show = "show";
   }
 };
 </script>
