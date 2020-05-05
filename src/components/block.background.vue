3e73aac32b87a38c30deb9d127c7e3ef3d8ce8d4
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-05-05 18:10:15 +0200

    Background optim

--- src/components/block.background.vue
+++ src/components/block.background.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="background">
     <img
-      src="../assets/page-background.jpg"
+      src="../assets/page-background-1x.jpg"
+      srcset="../assets/page-background-2x.jpg 2x"
       alt="Sink background"
       class="background__img"
+      rel="preload"
     />
   </section>
 </template>
@@ -11,3 +13,7 @@
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+@import "../css/block.background.scss";
+</style>
